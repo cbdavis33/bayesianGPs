7169e11c928c978f217d1d1bcbb3198b6c5f3d99
Casey Davis <casey.davis@merck.com>
2018-01-10 15:17:19 -0500

    It should all work as long as d > 1. For d = 1, the older code should be used. I'll do all that in another branch.
    Speed might be helped by changing x froma matrix to an array, vector, array of vectors, or vector of arrays. That's for a further branch.

--- dGreaterThan1/rCode/krigingRhoParam.R
+++ dGreaterThan1/rCode/krigingRhoParam.R
@@ -14,14 +14,15 @@ options(mc.cores = numCores)      # Then use one less than that for MCMC samplin
 
 sim_data_model <- stan_model(file = 'dGreaterThan1/stanCode/generateKrigingDataRhoParam.stan')
 
-sigma <- 1
-rho <- c(0.15, 0.6)
-sigmaEps <- sqrt(0.1)
 mu <- 5
+rho <- c(0.15, 0.6)
+sigma <- 0.5
+sigmaEps <- sqrt(0.1)
+
 d <- length(rho)
 
 
-dat_list <- list(n = 1000, D = d, mu = mu, sigma = sigma, rho = rho, sigmaEps = sigmaEps)
+dat_list <- list(n = 300, D = d, mu = mu, sigma = sigma, rho = rho, sigmaEps = sigmaEps)
 set <- sample(1:dat_list$n, size = 30, replace = F)
 # draw <- sampling(sim_data_model, iter = 1, algorithm = 'Fixed_param', chains = 1, data = dat_list,
 #                  seed = 363360090)
@@ -50,21 +51,21 @@ stan_data <- list(n = length(set), nPred = dat_list$n - length(set),
 comp_gp_mod_lat <- stan_model(file = 'dGreaterThan1/stanCode/krigingRhoParam.stan')
 gp_mod_lat <- sampling(comp_gp_mod_lat, 
                        data = stan_data, 
-                       cores = 4, 
-                       chains = 4, 
-                       iter = 1000, 
+                       cores = 1, 
+                       chains = 1, 
+                       iter = 100, 
                        control = list(adapt_delta = 0.999))
 
 parsToMonitor <- c("mu", "rho", "sigma", "sigmaEps")
 print(gp_mod_lat, pars = parsToMonitor)
 
-# samps_gp_mod_lat <- extract(gp_mod_lat)
-# post_pred <- data.frame(x = stan_data$xPred,
-#                         pred_mu = colMeans(samps_gp_mod_lat$fPred))
+samps_gp_mod_lat <- extract(gp_mod_lat)
+post_pred <- data.frame(x = stan_data$xPred,
+                        pred_mu = colMeans(samps_gp_mod_lat$fPred))
+
 # plt_df_rt = data.frame(x = stan_data$xPred, f = t(samps_gp_mod_lat$fPred))
-# 
-# plt_df_rt_melt = melt(plt_df_rt,id.vars = 'x')
-# 
+# plt_df_rt_melt = melt(plt_df_rt,id.vars =)
+
 # p <- ggplot(data = plt_df[set,], aes(x=x, y=y)) +
 #   geom_line(data = plt_df_rt_melt, aes(x = x, y = value, group = variable, color = "Posterior draws")) + 
 #   geom_point(aes(colour = 'Realized data')) +
@@ -83,56 +84,56 @@ print(gp_mod_lat, pars = parsToMonitor)
 # p
 # 
 # 
-# ppc_interval_df <- function(yrep, y) {
-#   q_95 <- apply(yrep,2,quantile,0.95)
-#   q_75 <- apply(yrep,2,quantile,0.75)
-#   q_50 <- apply(yrep,2,median)
-#   q_25 <- apply(yrep,2,quantile,0.25)
-#   q_05 <- apply(yrep,2,quantile,0.05)
-#   mu <- colMeans(yrep)
-#   df_post_pred <- data.frame(y_obs = y,
-#                              q_95 = q_95,
-#                              q_75 = q_75,
-#                              q_50 = q_50,
-#                              q_25 = q_25,
-#                              q_05 = q_05,
-#                              mu = mu)
-#   return(df_post_pred)
-# }
-# ppc_interval_norm_df <- function(means, sds, y) {
-#   q_95 <- qnorm(0.95,mean = means, sd = sds)
-#   q_75 <- qnorm(0.75,mean = means, sd = sds)
-#   q_50 <- qnorm(0.5,mean = means, sd = sds)
-#   q_25 <- qnorm(0.25,mean = means, sd = sds)
-#   q_05 <- qnorm(0.05,mean = means, sd = sds)
-#   df_post_pred <- data.frame(y_obs = y,
-#                              q_95 = q_95,
-#                              q_75 = q_75,
-#                              q_50 = q_50,
-#                              q_25 = q_25,
-#                              q_05 = q_05,
-#                              mu = means)
-#   return(df_post_pred)
-# }
-# interval_cover <- function(upper, lower, elements) {
-#   return(mean(upper >= elements & lower <= elements))
-# }
-# ppc_full_bayes <- ppc_interval_df(samps_gp_mod_lat$yPred, samps$y[1,-set])
-# 
-# coverage90 <- interval_cover(upper = ppc_full_bayes$q_95,
-#                              lower = ppc_full_bayes$q_05,
-#                              elements = ppc_full_bayes$y_obs)
-# coverage50 <- interval_cover(upper = ppc_full_bayes$q_75,
-#                              lower = ppc_full_bayes$q_25,
-#                              elements = ppc_full_bayes$y_obs)
-# 
-# print(c(coverage90, coverage50))
-# 
-# post <- as.data.frame(gp_mod_lat)
-# tmp <- select(post, mu, rho, sigma, sigmaEps)
-# bayesplot::mcmc_recover_hist(tmp, true = c(mu, rho, sigma, sigmaEps),
-#                              facet_args = list(ncol = 1))
-# 
+ppc_interval_df <- function(yrep, y) {
+  q_95 <- apply(yrep,2,quantile,0.95)
+  q_75 <- apply(yrep,2,quantile,0.75)
+  q_50 <- apply(yrep,2,median)
+  q_25 <- apply(yrep,2,quantile,0.25)
+  q_05 <- apply(yrep,2,quantile,0.05)
+  mu <- colMeans(yrep)
+  df_post_pred <- data.frame(y_obs = y,
+                             q_95 = q_95,
+                             q_75 = q_75,
+                             q_50 = q_50,
+                             q_25 = q_25,
+                             q_05 = q_05,
+                             mu = mu)
+  return(df_post_pred)
+}
+ppc_interval_norm_df <- function(means, sds, y) {
+  q_95 <- qnorm(0.95,mean = means, sd = sds)
+  q_75 <- qnorm(0.75,mean = means, sd = sds)
+  q_50 <- qnorm(0.5,mean = means, sd = sds)
+  q_25 <- qnorm(0.25,mean = means, sd = sds)
+  q_05 <- qnorm(0.05,mean = means, sd = sds)
+  df_post_pred <- data.frame(y_obs = y,
+                             q_95 = q_95,
+                             q_75 = q_75,
+                             q_50 = q_50,
+                             q_25 = q_25,
+                             q_05 = q_05,
+                             mu = means)
+  return(df_post_pred)
+}
+interval_cover <- function(upper, lower, elements) {
+  return(mean(upper >= elements & lower <= elements))
+}
+ppc_full_bayes <- ppc_interval_df(samps_gp_mod_lat$yPred, samps$y[1,-set])
+
+coverage90 <- interval_cover(upper = ppc_full_bayes$q_95,
+                             lower = ppc_full_bayes$q_05,
+                             elements = ppc_full_bayes$y_obs)
+coverage50 <- interval_cover(upper = ppc_full_bayes$q_75,
+                             lower = ppc_full_bayes$q_25,
+                             elements = ppc_full_bayes$y_obs)
+
+print(c(coverage90, coverage50))
+
+post <- as.data.frame(gp_mod_lat)
+tmp <- select(post, mu, "rho[1]", "rho[2]", sigma, sigmaEps)
+bayesplot::mcmc_recover_hist(tmp, true = c(mu, rho[1], rho[2], sigma, sigmaEps),
+                             facet_args = list(ncol = 1))
+
 # ppc_full_bayes$x <- samps$x[1,-set]
 # ggplot(data = ppc_full_bayes, aes(x = x, y = y_obs)) +
 #   geom_ribbon(aes(ymax = q_95, ymin = q_05,alpha=0.5, colour = '90% predictive interval')) + 
